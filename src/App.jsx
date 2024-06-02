60a7b5bf3037f85aac34abdc7bcc1e6df497be50
tj <tirtheshjaintj@gmail.com>
2024-06-02 17:20:17 +0530

    Bg Fixes with new UI changes and more Cookie Time

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate ,Outlet} from 'react-router-dom';
-import Cookie from 'universal-cookie';
-import axios from 'axios';
-import { addUser } from './store/userSlice';
+import React from 'react';
+import {Outlet} from 'react-router-dom';
 import Navbar from './components/Navbar';
 
 function App() {
-  const user = useSelector(state => state.userData);
-  const navigate = useNavigate();
-  useEffect(() => {
-      const cookie = new Cookie();
-      const token = cookie.get('token');
-      if (!token) {
-        navigate('/login');
-        return;
-      }
-    
-  }, [navigate]);
 
   return (
     <>
