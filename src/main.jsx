e0559834c4ad461bca4ed756e236039ca9933156
tj <tirtheshjaintj@gmail.com>
2024-12-04 10:14:26 +0530

    Login FIxed

--- src/main.jsx
+++ src/main.jsx
@@ -32,3 +32,15 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </GoogleOAuthProvider>
 )
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('serviceWorker.js')
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope);
+      })
+      .catch(err => {
+        console.error('Service Worker registration failed:', err);
+      });
+  });
+}
