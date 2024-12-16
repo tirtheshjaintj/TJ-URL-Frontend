083ba3efb08f50b07bc05eb1b7379263e26a7cd0
tj <tirtheshjaintj@gmail.com>
2024-12-16 23:13:05 +0530

    TimeStamps

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -100,7 +100,7 @@ function Dashboard() {
         else if (days > 0) return `${days} day${days > 1 ? "s" : ""} ago`;
         else if (hours > 0) return `${hours} hour${hours > 1 ? "s" : ""} ago`;
         else if (minutes > 0) return `${minutes} minute${minutes > 1 ? "s" : ""} ago`;
-        return `${seconds} second${seconds > 1 ? "s" : ""} ago`;
+        return `Just Now`;
     }
 
     const createChartData = (field) => {
